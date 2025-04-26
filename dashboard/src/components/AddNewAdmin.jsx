d9aaae72816fdd7474bbae884d45a7d1868c9b70
Bikram Singh <bikramsinghsaini046@gmail.com>
2025-04-26 08:11:07 +0530

    localhost 4000

--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "http://localhost:5174/api/v1/user/admin/addnew",
+          "http://localhost:4000/api/v1/user/admin/addnew",
           { firstName, lastName, email, phone, aadhar, dob, gender, password },
           {
             withCredentials: true,
